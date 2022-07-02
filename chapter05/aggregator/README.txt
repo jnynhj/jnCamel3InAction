8769e027219c58b6db4bc47afc578cba8a0f8617
Jinnan Ning <jnynhj@yahoo.com.au>
2022-07-02 13:10:53 +1000

    Add testcases for completion conditions for the aggregator

--- chapter05/aggregator/README.txt
+++ chapter05/aggregator/README.txt
@@ -6,6 +6,27 @@ AggregateABCTest: P150
   *  Dependency: camel-core
   *  Run: mvn test -Dtest=AggregateABCTest
 
+AggregateXMLTest: P154
+  *  The Aggregator EIP using multiple completion conditions.
+     -  ${property.xxx} is deprecated in 3.x. Use ${exchangeProperty.xxx} which is the correct name for the function.
+  *  EIP: Aggregator EIP
+  *  DSL: aggregate(), xpath(), completionSize(), complettionTimeout()
+  *  Annotation: N/A
+  *  Dependency: camel-core
+  *  Run: mvn test -Dtest=AggregateXMLTest
+
+Others: p157
+  *  Additional configuration options for the Aggregator EIP:
+        mvn test -Dtest=AggregateABCEagerTest
+        mvn test -Dtest=SpringAggregateABCEagerTest
+        mvn test -Dtest=AggregateABCCloseTest
+        mvn test -Dtest=SpringAggregateABCCloseTest
+        mvn test -Dtest=AggregateABCInvalidTest
+        mvn test -Dtest=SpringAggregateABCInvalidTest
+        mvn test -Dtest=AggregateABCGroupTest
+        mvn test -Dtest=SpringAggregateABCGroupTest
+        mvn test -Dtest=AggregateTimeoutThreadpoolTest
+        mvn test -Dtest=SpringAggregateTimeoutThreadpoolTest
 
 To run a test class:
         mvn test -Dtest=<testClassName>
---
+++ chapter05/aggregator/src/test/java/camelinaction/AggregateABCGroupTest.java
@@ -0,0 +1,98 @@
+package camelinaction;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.processor.aggregate.GroupedExchangeAggregationStrategy;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * The ABC example for using the Aggregator EIP.
+ * <p/>
+ * This example have 4 messages send to the aggregator, by which one
+ * message is published which groups the incoming messages.
+ * <p/>
+ * And this time we group the incoming messages which means all 4 incoming Exchange
+ * is kept in a List on the published Exchange. The List is stored as a property
+ * which allows you to get access to those original incoming Exchanges.
+ */
+public class AggregateABCGroupTest extends CamelTestSupport {
+
+    @SuppressWarnings("unchecked")
+	@Test
+    public void testABCGroup() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        // one message expected
+        mock.expectedMessageCount(2);
+        // As the fix of CAMEL-6557, the message body is not empty anymore
+        // should not have a body
+        //mock.message(0).body().isNull();
+        // but have it stored in a property as a List
+//        mock.message(0).exchangeProperty(Exchange.GROUPED_EXCHANGE).isInstanceOf(List.class);
+
+        template.sendBodyAndHeader("direct:start", "A", "myId", 1);
+        template.sendBodyAndHeader("direct:start", "B", "myId", 1);
+        template.sendBodyAndHeader("direct:start", "F", "myId", 2);
+        template.sendBodyAndHeader("direct:start", "C", "myId", 1);
+        template.sendBodyAndHeader("direct:start", "G", "myId", 2);
+        template.sendBodyAndHeader("direct:start", "H", "myId", 2);
+        template.sendBodyAndHeader("direct:start", "I", "myId", 3);
+
+        assertMockEndpointsSatisfied();
+
+        // get the published exchange
+        Exchange exchange = mock.getExchanges().get(0);
+        Exchange exchange1 = mock.getExchanges().get(1);
+
+//        System.out.println("JNDBG1: " + exchange.getIn());
+//        System.out.println("JNDBG1: " + exchange.getIn().getBody());
+//        System.out.println("JNDBG3: " + mock.message(0));
+        // retrieve the List which contains the arrived exchanges
+//        List list = exchange.getProperty(Exchange.GROUPED_EXCHANGE, List.class);
+//        assertEquals("Should contain the 3 arrived exchanges", 3, list.size());
+        List list = exchange.getIn().getBody(List.class);
+        assertEquals(3, list.size());
+
+//        List list1 = exchange1.getProperty(Exchange.GROUPED_EXCHANGE, List.class);
+//         assertEquals("Should contain the 3 arrived exchanges", 3, list1.size());
+        List list1 = exchange1.getIn().getBody(List.class);
+        assertEquals(3, list1.size());
+
+        // assert the 3 exchanges are in order and contains the correct body
+        Exchange a = (Exchange) list.get(0);
+        assertEquals("A", a.getIn().getBody());
+
+        Exchange a1 = (Exchange) list1.get(0);
+        assertEquals("F", a1.getIn().getBody());
+
+
+        Exchange b = (Exchange) list.get(1);
+        assertEquals("B", b.getIn().getBody());
+
+        Exchange c = (Exchange) list.get(2);
+        assertEquals("C", c.getIn().getBody());
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                    // do a little logging
+                    .log("Sending ${body} with correlation key ${header.myId}")
+                    // aggregate based on header correlation key
+                    .aggregate(header("myId"), new GroupedExchangeAggregationStrategy()).completionSize(3)
+                        // do a little logging for the published message
+                        .log("Sending out ${body}")
+                        // and send it to the mock
+                        .to("mock:result");
+            }
+        };
+    }
+}
